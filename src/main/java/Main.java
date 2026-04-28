f869dbe644f2d2b1afe753790634eaaaa3db4735
Jaime <zokan300524@gmail.com>
2026-04-28 06:14:32 +0200

    añade estructura del proyecto y mejora salida

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 public class Main {
     public static void main(String[] args) { System.out.println("MiniInventario iniciado correctamente.");
     }
+    System.out.println("Versión inicial con gestión básica de productos.");
 }
